7040ebd0a1ba5a5e0a617020c3faf7b569703b53
Erick Ho <eho@function.network>
2024-10-06 14:30:01 -0500

    add seperate api client func

--- src/index.ts
+++ src/index.ts
@@ -4,26 +4,30 @@ import {
     APIGatewayService
 } from "@buf/fxnlabs_api-gateway.connectrpc_es/apigateway/v1/apigateway_connect.js";
 
+function createAPIClient() {
+    // Adds API Key to each request
+    const apiKeyInterceptor: Interceptor = (next) => async (req) => {
+        req.header.set("x-api-key", process.env.FXN_API_KEY!)
+        return await next(req);
+    };
 
-// Adds API Key to each request
-const apiKeyInterceptor: Interceptor = (next) => async (req) => {
-    req.header.set("x-api-key", process.env.FXN_API_KEY!)
-    return await next(req);
-};
+    // Creates the transport
+    const transport = createConnectTransport({
+        httpVersion: "1.1",
+        baseUrl: "api.function.network",
+        interceptors: [apiKeyInterceptor]
+    });
 
-// Creates the transport
-const transport = createConnectTransport({
-    httpVersion: "1.1",
-    baseUrl: "api.function.network",
-    interceptors: [apiKeyInterceptor]
-});
+    // Creates the client
+    return createPromiseClient(APIGatewayService, transport);
+}
 
-// Creates the client
-const apiClient = createPromiseClient(APIGatewayService, transport);
-
-
-// Demo end to end
 async function main() {
+
+    // Init the apiClient (should only be called once)
+    const apiClient = createAPIClient()
+
+    // Use API Client
     const response = await apiClient.embed({
         model: "bge-small-en-v1.5",
         input: "Embed me so I can use it for RAG Pipelines",
